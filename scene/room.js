978d2e35c9ef7083dc07f6ee96a9545bced221b2
planner317 <planner317@gmail.com>
2020-05-26 17:23:37 +0300

    2321

--- scene/room.js
+++ scene/room.js
@@ -2,65 +2,66 @@ let ddd = new Ddd()
 ddd.animate();
 
 //////////////////////// менюшка ///////////////////////
-menuView.onclick=(e)=>{
+menuView.onclick = (e) => {
     e.stopPropagation()
     e.preventDefault();
     if (menu.style.visibility == "visible") menu.style.visibility = "hidden"
     else menu.style.visibility = "visible"
 }
-menu.onclick=(e)=>{
+menu.onclick = (e) => {
     e.stopPropagation()
     //e.preventDefault();
 }
-full.onclick=()=>{
+full.onclick = () => {
     if (document.fullscreen) document.exitFullscreen()
     else document.documentElement.requestFullscreen()
 }
-speedSet.oninput=()=>{
-    speedView.innerText="чувствительность сенсора " + speedSet.value;
+speedSet.oninput = () => {
+    speedView.innerText = "чувствительность сенсора " + speedSet.value;
     ddd.touchControl.speed = speedSet.value
 }
 
 window.addEventListener("mousedown", hideInfo)
 window.addEventListener("touchstart", hideInfo)
 
-function hideInfo(e){
+function hideInfo(e) {
     info.remove();
-    window.removeEventListener("mousedown",hideInfo)
-    window.removeEventListener("touchstart",hideInfo)
+    window.removeEventListener("mousedown", hideInfo)
+    window.removeEventListener("touchstart", hideInfo)
 }
-
-fps.onchange = ()=> {if (fps.checked) ddd.stats.setMode(0); else ddd.stats.setMode(); return false} 
+function setResolution(r) {
+    ddd.resolution = r
+    ddd.resize()
+}
+fps.onchange = () => { if (fps.checked) ddd.stats.setMode(0); else ddd.stats.setMode(); return false }
 
 //////////////////////// 3d пространство
 function Ddd() {
     let t = this
     t.scene = new THREE.Scene();
     t.scene.background = new THREE.Color(0x223344)
-
     t.renderer = new THREE.WebGLRenderer({ antialias: true });
-    t.renderer.setSize(window.innerWidth, window.innerHeight);
+    t.resolution = 720
     document.body.append(t.renderer.domElement);
 
     t.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    t.camera.position.y=25;
+    t.camera.position.y = 25;
 
     t.stats = new Stats()
-    t.stats.setMode()
-    document.body.append(t.stats.dom);
-    
+    fps.append(t.stats.dom);
+
     // движение мышкой в режиме pointerLock и движение клавой
     t.pointerLook = new THREE.MouseAndKeyboardControlsFirstPerson(t.camera, document.body);
 
     // уравление камерой с помощью сенсорного экрана, кидаем только камеру области для нажатия создает сам
-    t.touchControl = new TouchControl(t.camera); 
+    t.touchControl = new TouchControl(t.camera);
 
     t.mode; // режим упарвление сенсором или клавиатурой с мышкой
 
     var onProgress = function (xhr) {
         if (xhr.lengthComputable) {
             var percentComplete = xhr.loaded / xhr.total * 100;
-            download.style.lineHeight = window.innerHeight/1.5 +"px";
+            download.style.lineHeight = window.innerHeight / 1.5 + "px";
             download.innerHTML = `загрузка ${Math.round(percentComplete, 2)}%`;
         }
     };
@@ -71,40 +72,61 @@ function Ddd() {
 
     ///////////////////////
     new THREE.MTLLoader(manager)
-        .setPath('model/')
+        .setPath('model/room/')
         .load('room.mtl', function (materials) { // callmback функция вызывается после загрузки
 
             materials.preload();
 
             new THREE.OBJLoader(manager)
                 .setMaterials(materials)
-                .setPath('model/')
+                .setPath('model/room/')
                 .load('room.obj', function (object) {
                     t.room = object
+                    t.mirrorRoom= new THREE.Object3D();
+                    t.mirrorRoom.copy(t.room);
+                    t.mirrorRoom.scale.x=-1
+                    t.mirrorRoom.position.x=-105.4
                     t.scene.add(object)
+                    t.scene.add(t.mirrorRoom)
                     download.remove()
                 }, onProgress, onError);
         });
 
 
-    t.onWinResize = () => {
+    t.resize = () => {
         t.camera.aspect = window.innerWidth / window.innerHeight;
         t.camera.updateProjectionMatrix();
-        t.renderer.setSize(window.innerWidth, window.innerHeight);
+        let oritntac = window.innerWidth > window.innerHeight // 1 ланшафтная 0 портретная
+        let coef = oritntac ? window.innerHeight / t.resolution : window.innerWidth / t.resolution
+        t.renderer.setSize(window.innerWidth / coef, window.innerHeight / coef);
+        t.renderer.domElement.style.zoom = coef;
+        getRes();
     }
+
+    t.onWinResize = t.resize
+
+    function getRes() {
+        resView.innerHTML = "внешнее<br>"
+            + window.outerWidth + " x " + window.outerHeight
+            + "<br>внутрение<br>"
+            + window.innerWidth + " x " + window.innerHeight
+            + "<br>canvas<br>"
+            + t.renderer.domElement.width + " x " + t.renderer.domElement.height;
+    }
+    t.resize();
     window.addEventListener('resize', t.onWinResize, false);
 
-    window.addEventListener("touchstart", ()=> t.mode = "sensor");
+    window.addEventListener("touchstart", () => t.mode = "sensor");
 
     t.animate = () => {
 
         requestAnimationFrame(t.animate);
 
-       if (t.mode == "sensor") t.touchControl.update();
-       else t.pointerLook.update()
+        if (t.mode == "sensor") t.touchControl.update();
+        else t.pointerLook.update()
 
-       if (fps.checked) t.stats.update();
-       
+        if (menu.style.visibility == "visible") t.stats.update();
+
         t.renderer.render(t.scene, t.camera);
 
     }
